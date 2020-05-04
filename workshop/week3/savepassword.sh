17d43bae27c379d4b5d9dc3df953762b0b3f65d4
Ubuntu <labuser@ML-RefVm-262326.h54rnubfqvvexo03i3dcrjaq1h.px.internal.cloudapp.net>
2020-05-04 17:51:49 +0000

    new

--- workshop/week3/savepassword.sh
+++ workshop/week3/savepassword.sh
@@ -5,5 +5,10 @@ filepath=`find ~ -name "$folderName"`
 echo $filepath
 #prompt the user to enter a password
 read -s -p "enter a password:" password
+file="$filepath/secret.txt"
+if [ -f "$file" ]; then
+echo "$password" >> "$file"
+else
 echo "$password" > secret.txt
 mv secret.txt "$filepath"
+fi
